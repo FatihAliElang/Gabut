841f91642d459c953db753410e7f877a686c8237
ELs <110036508+fatihelang@users.noreply.github.com>
2022-10-07 09:21:33 +0700

    Update tebak.py

--- Game/tebak.py
+++ Game/tebak.py
@@ -1,13 +1,22 @@
 import random as abstrak
 acak = abstrak.randint(1, 100)
 
+print("=====Selamat Menebak=====")
+
 while True:
-    print("=====Selamat Menebak=====")
-    pilih = int(input("Tebak Angkanya: "))
-    if(pilih < acak):
-        print("Angka terlalu kecil")
-    elif(pilih > acak):
-        print("Angka terlalu besar")
-    else:
-        print("Tebakan anda benar")
-        break
+    try:
+        pilih = int(input("Tebak Angkanya: "))
+        if(pilih < acak):
+            print("Angka terlalu kecil")
+            print("")
+        elif(pilih > acak):
+            print("Angka terlalu besar")
+            print("")
+        else:
+            print("=====Tebakan anda benar=====")
+            break
+
+    except ValueError:
+        print("Ketik angka")
+        
+           
